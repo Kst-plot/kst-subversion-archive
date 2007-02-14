beccd8f65c109907f3d0fdcd0626f9a02343a144
Adam Treat <treat@kde.org>
2007-02-14 21:49:51 +0000

    * Add various CMakeLists.txt created by running the am2cmake script found in kdesdk/cmake in KDE svn.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/branches/work/kst@633700 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ portto4/ConvenienceLibs.cmake
@@ -0,0 +1,15 @@
+
+#former libtool convenience libraries:
+
+# kst/src/extensions/js/kjsembed/Makefile.am: kjsembed
+
+# kst/src/extensions/js/kjsembed/builtins/Makefile.am: kjsembedbuiltins
+
+# kst/src/extensions/js/kjsembed/bindings/Makefile.am: kjsembedbindings
+
+# kst/src/extensions/js/kjsembed/qtbindings/Makefile.am: kjsembedqtbindings
+
+# kst/src/datasources/libfitstools/Makefile.am: fitstools
+
+# kst/src/libkstkmdi/Makefile.am: kstkmdi
+
